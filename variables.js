45ab4c0b8c68a946914b02947e2434c0909c8e9f
Ruben Castillo <rcastillo@raivtech.com>
2020-04-08 22:20:13 -0600

    Variables en JS
    Variables en JS

---
+++ variables.js
@@ -0,0 +1,15 @@
+var nombre = "Ruben";
+
+// Declarar
+var edad;
+// Inicializar
+edad = 24;
+
+var elementos = ["Computadora", "Celular", "Tableta"];
+
+var persona = {
+    nombre: "Ruben",
+    edad: 24
+};
+
+persona;

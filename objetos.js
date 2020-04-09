fe2d12849ca469a029d40e62d005b70c3632f939
Ruben Castillo <rcastillo@raivtech.com>
2020-04-09 17:52:51 -0600

    Objetos en JS
    Objetos en JavaScript

---
+++ objetos.js
@@ -0,0 +1,11 @@
+//Objects
+
+var miAuto = {
+    marca: "Chevrolet",
+    modelo: "Corsa",
+    anio: 2006,
+    detalleAuto: function () {
+        console.log(`Auto ${this.modelo} ${this.anio}`);
+    }
+};
+

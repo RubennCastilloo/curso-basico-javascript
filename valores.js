55fa0e14325fb291ace0965808322e30f988f9a7
Ruben Castillo <rcastillo@raivtech.com>
2020-04-08 22:19:55 -0600

    Valores en JS
    Valores en JS

---
+++ valores.js
@@ -0,0 +1,11 @@
+//Elementos de un lenguaje de programación: Variables, Funciones y Sintáxis
+// Con typeof vemos que tipo de valores tenemos
+
+var int = 40; // Valores de tipo Int
+var string = "Rubén Castillo"; // Valores de tipo String
+var bool = true; // Valores de tipo booleano
+var placeholder = null; // Valores sin primitivos
+
+var array = [1, 2, 3]; // Valores de tipo arreglo
+var object = {nombre: "Ruben"}; // Valor tipo objeto
+

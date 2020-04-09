e9e11fcf5c7ba4cb03d2d89531eae030c0902285
Ruben Castillo <rcastillo@raivtech.com>
2020-04-09 16:28:35 -0600

    Arrays con JS
    Arreglos en JavaScript

---
+++ arrays.js
@@ -0,0 +1,17 @@
+var frutas = [
+    "Manzana",
+    "Platano",
+    "Cereza",
+    "Fresa"
+];
+
+var masFrutas = frutas.push("Uvas"); //Push agrega elementos al final del arreglo
+
+var ultimo = frutas.pop("Uvas"); //Pop elimina elementos del arreglo
+
+var nuevaLongitud = frutas.unshift("Uvas"); //Unshift agrega elementos al inicio del arreglo
+
+var borrarFruta = frutas.shift("Uvas"); //Shift nos borra el primer elemento del arreglo
+
+var posicion = frutas.indexOf("Manzana"); //IndexOf nos trae la posicion del elemento del arreglo
+

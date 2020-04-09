f4c170ddf5de6bd91920daf267710645285b478d
Ruben Castillo <rcastillo@raivtech.com>
2020-04-08 23:08:31 -0600

    Operadores: Asignacion, Comparacion y Aritmeticos
    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators

---
+++ operadores.js
@@ -0,0 +1,27 @@
+//Operadores: Asignacion, Comparacion y Aritmeticos
+
+//Valores binarios
+3 + 2;
+50 - 10;
+10 * 20;
+10 / 2;
+
+var nombre = "Ruben " + "Castillo"; //Concatenar
+
+!false; //Negacion
+
+3 == "3"; //Comparacion resultado en consola: True
+3 === "3"; //Comparacion resultado en consola: False
+
+5 < 3;
+5 > 3;
+5 <= 6;
+5 >= 6;
+
+a && b; //Valida si "a" y "b" son verdad
+a || b; //Valida si alguna de las dos son verdad
+
+var edad = 40;
+edad++; //Incrementa 1 al valor, resultado en consola: 41
+edad+=2; //Incrementa 2 al valor, resultado en consola: 42
+
